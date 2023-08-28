7d8e5f99e2afb9af0d4aab3e1e512a24ca715542
Victor Santele <victor.santele@gmail.com>
2023-08-28 21:10:29 +0200

    Add sitemap

--- src/ArsenalExtractor.Web/astro.config.mjs
+++ src/ArsenalExtractor.Web/astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://arsenalmenu.vsantele.dev/",
-  integrations: [mdx()]
+  integrations: [mdx(), sitemap()]
 });
